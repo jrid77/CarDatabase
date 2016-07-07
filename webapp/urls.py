0cc26b353b07d1fcbadb00ab95666b5fe8115d2b
sampcr <sampcr@rose-hulman.edu>
2016-07-07 17:25:46 -0400

    implemented starting of bar graphs and transfer of data from python to html

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,8 +4,7 @@ from .views import graph, play_count_by_month
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$', graph),
-    url(r'^api/play_count_by_month', play_count_by_month, name = 'play_count_by_month'),
+    url(r'^bar-graph.html/', views.graph, name = bar-graph.html/),
 ]
 
 
--- webapp/views.py
+++ webapp/views.py
@@ -10,14 +10,7 @@ from django.http import HttpResponse
 from .models import Play
 
 def graph(request):
-    return render(request,'graph/graph.html')
-
-def play_count_by_month(request):
-    data = Play.objects.all() \
-       .extra(select={'month': connections[Play.objects.db].ops.date_trunc_sql('month','date')}) \
-       .values('month') \
-       .annotate(count_items=Count('id'))
-    return JsonResponse(list(data),safe=False)
+    return render(request,'personal/bar-graph.html',{'content':#insertJsonhere)
 
 def importData():
     
