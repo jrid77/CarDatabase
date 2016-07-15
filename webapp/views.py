730cc04563f5553850bddf167005eba9aa098e63
sampcr <sampcr@rose-hulman.edu>
2016-07-15 14:17:31 -0400

    all buttons set up, just need database stuff with them

--- webapp/views.py
+++ webapp/views.py
@@ -15,23 +15,33 @@ def graph(request):
 
 def index(request):
 #    string = getData(request)
-    return HttpResponse("""<h2>HEY</h2>
+    return HttpResponse("""<h2>Index</h2>
                         <button onclick="location.href='http://127.0.0.1:8000/webapp/cars'">
-				click to go to cars</button>
+							Cars</button>
+						<button onclick="location.href='http://127.0.0.1:8000/webapp/manufacturer'">
+							Manufacturer</button>
+						<button onclick="location.href='http://127.0.0.1:8000/webapp/transmission'">
+							Transmission</button>
+						<button onclick="location.href='http://127.0.0.1:8000/webapp/engine'">
+							Engine</button>
+						<button onclick="location.href='http://127.0.0.1:8000/webapp/emissions'">
+							Emissions</button>
+						<button onclick="location.href='http://127.0.0.1:8000/webapp/tows'">
+							Tows</button>
                         """)
 #    conn = MySQLdb.connect (host ="127.0.0.1",
 #                        user = "alexanjs",
 #                        passwd = "D@t@b@ses333",
 #			db = "sampledb")
 
-def createHTMLRow(line):
+def createCarHTMLRow(line):
 	rowText = "<tr>"
 	for element in line:
 		rowText += "<td>" + str(element) + "</td>"
 	rowText += "</tr>"
 	return rowText
 
-def getTable():
+def getCarTable():
 	db = MySQLdb.connect("localhost", "root", "D@t@b@ses333", "CarDatabase")
 	
 	cursor = db.cursor()
@@ -50,5 +60,31 @@ def getTable():
 
 
 def cars(request):
-	table = getTable()
+	HttpResponse("""<g2>Cars</h2>""")
+	table = getCarTable()
+	return HttpResponse(table)
+
+def manufacturer(request):
+	HttpResponse("""<g2>Cars</h2>""")
+	table = getCarTable()
+	return HttpResponse(table)
+
+def transmission(request):
+	HttpResponse("""<g2>Cars</h2>""")
+	table = getCarTable()
+	return HttpResponse(table)
+
+def engine(request):
+	HttpResponse("""<g2>Cars</h2>""")
+	table = getCarTable()
+	return HttpResponse(table)
+
+def emissions(request):
+	HttpResponse("""<g2>Cars</h2>""")
+	table = getCarTable()
+	return HttpResponse(table)
+
+def tows(request):
+	HttpResponse("""<g2>Cars</h2>""")
+	table = getCarTable()
 	return HttpResponse(table)
